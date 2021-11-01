f8207dbaec438b98d7ae23433fbd8988eaf4c797
Tarig Bouazzati <tbouzza@gmail.com>
2021-11-01 11:31:26 +0400

    CRUD actions implemented

---
+++ app/controllers/restaurants_controller.rb
@@ -0,0 +1,51 @@
+class RestaurantsController < ApplicationController
+  before_action :set_restaurant, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @restaurants = Restaurant.all
+  end
+
+  def show
+    # What do we have at this point?
+    # We have the params only!
+
+    # retrieve the restaurant based on the id
+
+    # What do we have at this point ?
+    # We have an instance of Restaurant
+  end
+
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+    @restaurant.save
+    redirect_to restaurant_path(@restaurant)
+  end
+
+  def edit
+    # display to a form with data about a specific restaurant
+  end
+
+  def update
+    @restaurant.update(restaurant_params)
+    redirect_to @restaurant
+  end
+
+  def destroy
+    @restaurant.destroy
+    redirect_to restaurants_path
+  end
+
+  private
+
+  def set_restaurant
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address, :rating)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,20 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # get '/restaurants', to: 'restaurants#index'
+
+  # get '/restaurants/new', to: 'restaurants#new', as: :new_restaurant
+
+  # get '/restaurants/:id', to: 'restaurants#show', as: :restaurant
+
+  # # Destroy a restaurant
+  # delete '/restaurants/:id', to: 'restaurants#destroy'
+
+  # # Create a restaurant
+  # post '/restaurants', to: 'restaurants#create'
+
+  # # Update a restaurant
+  # get '/restaurants/:id/edit', to: 'restaurants#edit', as: :edit_restaurant
+  # patch '/restaurants/:id', to: 'restaurants#update'
+
+  resources :restaurants
 end
